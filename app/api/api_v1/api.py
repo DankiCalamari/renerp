b82030a25d81a2e6f9a1d40779df200f69c63788
DankiCalamari <72004695+DankiCalamari@users.noreply.github.com>
2025-03-08 10:10:33 +1100

    Delete app directory

--- app/api/api_v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.api_v1.endpoints import users, inventory, sales, purchase
-
-api_router = APIRouter()
-api_router.include_router(users.router, tags=["users"])
-api_router.include_router(inventory.router, prefix="/inventory", tags=["inventory"])
-api_router.include_router(sales.router, prefix="/sales", tags=["sales"])
-api_router.include_router(purchase.router, prefix="/purchase", tags=["purchase"]) 
--- app/db/base.py
+++
@@ -1,4 +0,0 @@
-from app.db.base_class import Base
-from app.models.user import User
-from app.models.inventory import Category, Product, Stock, StockMovement
-from app.models.sales import Customer, Order, OrderItem, Invoice, Payment 
